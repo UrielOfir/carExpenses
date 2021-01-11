4b19aefa4a4d02dc6099a20a3ae5df030b6ac88b
UrielOfir <urielofir@gmail.com>
2021-01-11 10:49:23 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+registerBtn= document.querySelector("#registerBtn");
+
+
+registerBtn.addEventListener("click", () => {
+  insert();
+});
+
+function insert() {
+  const firstName = document.querySelector("#firstName");
+  const familyName = document.querySelector("#familyName");
+  const model = document.querySelector("#model");
+  const year = document.querySelector("#year");
+
+  const customer={
+
+  }
+}
+
